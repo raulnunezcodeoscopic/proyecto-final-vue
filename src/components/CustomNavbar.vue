cfe7eac1df85f5b74135dbe391ee64650121cb0c
Raúl Núñez <raul.nunez@codeoscopic.com>
2022-11-24 10:51:16 +0100

    Add logo to header

--- src/components/CustomNavbar.vue
+++ src/components/CustomNavbar.vue
@@ -1,9 +1,30 @@
 <template>
-    <nav>
-        <div>
-            <router-link to="/">Inicio</router-link> |
-            <router-link to="/contact">Contacto</router-link> |
-            <router-link to="/author">Autor</router-link>
+    <header>
+        <div class='logo'>
+            <img src="../assets/logo.png" />
         </div>
-    </nav>
+        <nav>
+            <div>
+                <router-link to="/">Inicio</router-link> |
+                <router-link to="/contact">Contacto</router-link> |
+                <router-link to="/author">Autor</router-link>
+            </div>
+        </nav>
+    </header>
 </template>
+<style scoped>
+    
+    header {
+        display: flex;
+        justify-content: space-between;
+    }
+    
+    .logo img {
+        width: 60px;
+        padding: 10px;
+        object-position: center;
+        object-fit: cover;
+        margin-top: 10px;
+    }
+
+</style>
